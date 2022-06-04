48ad69dfb8f46596261eae353d6f745abeefc08a
Silpakorumban <102651370+Silpakorumban@users.noreply.github.com>
2022-06-05 03:00:45 +0530

    m,

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 // to remove the warning (Current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version.) i have added  useNewUrlParser: true,
 // useUnifiedTopology: true,
 //     useNewUrlParser: true
-mongoose.connect('mongodb://localhost:27017/Library', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-});
+mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect('mongodb://localhost:27017/Library');
+//  {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// });
 const Schema = mongoose.Schema;
 
 
